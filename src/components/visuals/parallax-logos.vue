948ccedd53273ccc67f5c525c5d70374fc68a749
César MORO <cesar.moro.dev@gmail.com>
2023-04-06 21:25:19 +0200

    Retouches sur les dimensions et positions des logos pour la parallax

--- src/components/visuals/parallax-logos.vue
+++ src/components/visuals/parallax-logos.vue
@@ -7,7 +7,7 @@
         <img class="logos logo__javascript rellax" data-rellax-speed="-2" data-rellax-zindex="3" src="../../assets/img/logos/javascript.svg"/>
         <img class="logos logo__node rellax" data-rellax-speed="5" data-rellax-zindex="" src="../../assets/img/logos/nodejs.svg"/>
         <img class="logos logo__php rellax" data-rellax-speed="4" data-rellax-zindex="" src="../../assets/img/logos/php.svg"/>
-        <img class="logos logo__mysql rellax" data-rellax-speed="3" data-rellax-zindex="" src="../../assets/img/logos/mysql.svg"/>
+        <img class="logos logo__mysql rellax" data-rellax-speed="6" data-rellax-zindex="" src="../../assets/img/logos/mysql.svg"/>
         <img class="logos logo__wordpress rellax" data-rellax-speed="1" data-rellax-zindex="" src="../../assets/img/logos/wordpress-icon.svg"/>
         <img class="logos logo__github rellax" data-rellax-speed="7" data-rellax-zindex="" src="../../assets/img/logos/github-icon.svg"/>
 </div>
